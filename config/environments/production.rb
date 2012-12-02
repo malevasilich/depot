66be29bec957920760767f401484e3f7d9054833
malevasilich <malevasilich@gmail.com>
2012-12-02 20:51:26 +0600

    gmail settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,14 +51,16 @@ Depot::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:  "smpt.example.com"
-    port:     25
-    domain:   example.com
-    authentication: "plain"
-    user_name: "storemailer"
-    password: "secret"
+    address:  "smtp.gmail.com",
+    port:     "587",
+    domain:   "gmail.com",
+    authentication: "plain",
+    user_name: "malevasilich@gmail.com",
+    password: "secret",
+    enable_starttls_auto: true
   }
 
   # Enable threaded mode
